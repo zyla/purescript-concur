8f3cccc7301d87d008352debb5628faabfae64c2
Maciej Bielecki <zyla@prati.pl>
2017-11-01 22:52:50 +0100

    rm Main

--- src/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* jshint browser: true */
-
-// getElementById :: String -> Eff eff Node
-exports.getElementById = function(id) {
-  return function() {
-    return document.getElementById(id);
-  };
-};
